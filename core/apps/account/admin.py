eff6bff65ff39896a6bb42dab7049c5b1ae23b11
nape <nap3ntsoane@protonmail.com>
2025-01-12 23:09:22 +0200

    Knox extraction

--- core/apps/account/admin.py
+++ core/apps/account/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 from .models import Account
-from knox.models import AuthToken
 
 # Register your models here.
 # pg 78 docs
--- core/apps/account/views.py
+++ core/apps/account/views.py
@@ -9,7 +9,6 @@ from rest_framework import (
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
-from knox.models import AuthToken
 
 from .serializers import (
 	AccountSerializer,
