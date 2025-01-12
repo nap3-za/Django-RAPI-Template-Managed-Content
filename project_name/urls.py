408aa183b4369ae16d0b487c5fc6acb535f8ca18
nape <nap3ntsoane@protonmail.com>
2025-01-12 22:55:22 +0200

    Useless imports remove

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
 
 from core.apps.account import views as account_views
 
